5afc10cca49ffaebe78846ed3e151dc55595a051
Gabriel Bessa <gbessa@avenuecode.com>
2016-09-06 13:49:18 -0300

    First config created

---
+++ initial_config.sh
@@ -0,0 +1,16 @@
+# Installing Vim
+sudo apt-get install vim
+
+# Installing Sublime-Text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+# Installing Compiz
+sudo apt-get install compiz compizconfig-settings-manager compiz-fusion-plugins-extra compiz-fusion-plugins-main compiz-plugins
+
+# Installing Google-chrome stable
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
