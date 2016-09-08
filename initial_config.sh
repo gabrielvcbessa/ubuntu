460f9647540c21b87399e678d89f02b5efcd8b4e
Gabriel Bessa <gbessa@avenuecode.com>
2016-09-08 09:49:43 -0300

    Spotify added

--- initial_config.sh
+++ initial_config.sh
@@ -17,3 +17,9 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update 
 sudo apt-get install google-chrome-stable
+
+# Installing Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
